33375a55faec720dadb6083f1d013508d64b3217
Pipe Grind <drpipe1098@gmail.com>
2026-09-17 22:43:56 -0500

    feat: add admin system status controller

---
+++ app/Http/Controllers/Admin/SystemController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+use Throwable;
+
+class SystemController extends Controller
+{
+    public function __invoke(Request $request): View
+    {
+        $user = $request->user();
+
+        abort_unless(
+            $user instanceof User && $user->isPlatformAdmin(),
+            403,
+        );
+
+        $databaseOnline = false;
+
+        try {
+            DB::connection()->select('select 1');
+            $databaseOnline = true;
+        } catch (Throwable) {
+            $databaseOnline = false;
+        }
+
+        return view('admin.system', [
+            'databaseOnline' => $databaseOnline,
+            'databaseDriver' => DB::connection()->getDriverName(),
+            'environment' => app()->environment(),
+            'laravelVersion' => app()->version(),
+            'queueConnection' => (string) config('queue.default'),
+            'sessionDriver' => (string) config('session.driver'),
+        ]);
+    }
+}
