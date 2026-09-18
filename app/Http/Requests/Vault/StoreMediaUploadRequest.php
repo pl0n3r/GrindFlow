bb86e9aa953948e1ce4a806d92621c44f367cf2e
Pipe Grind <drpipe1098@gmail.com>
2026-09-17 23:39:36 -0500

    security: enforce safe manual upload size

--- app/Http/Requests/Vault/StoreMediaUploadRequest.php
+++ app/Http/Requests/Vault/StoreMediaUploadRequest.php
@@ -31,7 +31,7 @@ class StoreMediaUploadRequest extends FormRequest
                 'required',
                 'file',
                 'mimetypes:'.implode(',', $mimetypes),
-                'max:'.(int) config('grindflow.media.max_upload_kb', 512000),
+                'max:'.(int) config('grindflow.media.max_upload_kb', 7168),
             ],
         ];
     }
