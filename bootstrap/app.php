9506bdbfd73a1a99cd7e54d72676b810bcd2af77
Pipe Grind <drpipe1098@gmail.com>
2026-09-17 23:33:10 -0500

    feat: register organization tenant middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\ApplyTenantUserContext;
+use App\Http\Middleware\ResolveOrganizationContext;
 use App\Support\Diagnostics\DiagnosticLog;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -17,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'tenant.user' => ApplyTenantUserContext::class,
+            'tenant.organization' => ResolveOrganizationContext::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
