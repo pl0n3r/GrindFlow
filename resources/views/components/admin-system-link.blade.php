4dcc161f2b070fe4687f8e0488fec29f3314b142
Pipe Grind <drpipe1098@gmail.com>
2026-09-17 22:52:04 -0500

    chore: remove redundant admin navigation partial

--- resources/views/components/admin-system-link.blade.php
+++
@@ -1,8 +0,0 @@
-@if (auth()->user()?->isPlatformAdmin())
-    <span class="gf-sidebar__label">Admin</span>
-
-    <a class="gf-navitem" href="{{ route('admin.system') }}">
-        <span class="gf-navitem__icon" aria-hidden="true">⌘</span>
-        <span class="gf-navitem__text">System</span>
-    </a>
-@endif
