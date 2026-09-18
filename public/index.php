f2bc465c8f477ac996a195908768d6260cb67761
Pipe Grind <drpipe1098@gmail.com>
2026-09-18 00:11:49 -0500

    style: align bootstrap guard instantiation

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 
 require __DIR__.'/../vendor/autoload.php';
 
-$cacheGuard = new ReleaseCacheGuard();
+$cacheGuard = new ReleaseCacheGuard;
 
 if (
     $cacheGuard->refreshIfNeeded(
