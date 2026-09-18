23bb12bf5398c60dbd2525bf934eb66452efde96
Pipe Grind <drpipe1098@gmail.com>
2026-09-17 22:51:19 -0500

    feat: link admin system status from dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -44,6 +44,15 @@
                     <span class="gf-navitem__text">Analytics</span>
                 </span>
 
+                @if ($isPlatformAdmin)
+                    <span class="gf-sidebar__label">Admin</span>
+
+                    <a class="gf-navitem" href="{{ route('admin.system') }}">
+                        <span class="gf-navitem__icon" aria-hidden="true">⌘</span>
+                        <span class="gf-navitem__text">System</span>
+                    </a>
+                @endif
+
                 <x-admin-system-link />
             </nav>
 
