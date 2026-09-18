9938c65a28a3325284764867be2255ba502d4fb8
Pipe Grind <drpipe1098@gmail.com>
2026-09-17 23:33:12 -0500

    feat: route organization media vault

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\DiagnosticsController;
 use App\Http\Controllers\Admin\SystemController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Vault\VaultController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
@@ -17,6 +18,16 @@ Route::middleware('guest')->group(function (): void {
 Route::middleware(['auth', 'tenant.user'])->group(function (): void {
     Route::get('/dashboard', DashboardController::class)
         ->name('dashboard');
+
+    Route::prefix('/organizations/{organizationId}')
+        ->middleware('tenant.organization')
+        ->group(function (): void {
+            Route::get('/vault', [VaultController::class, 'index'])
+                ->name('organizations.vault.index');
+            Route::post('/vault', [VaultController::class, 'store'])
+                ->name('organizations.vault.store');
+        });
+
     Route::get('/admin/system', SystemController::class)
         ->name('admin.system');
     Route::get('/admin/diagnostics', [DiagnosticsController::class, 'index'])
