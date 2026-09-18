32556ad021010f0599b9154455668b43fb695ec2
Pipe Grind <drpipe1098@gmail.com>
2026-09-18 08:58:14 -0500

    feat: add safe media connector errors

---
+++ app/Services/Media/Connectors/MediaConnectorException.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services\Media\Connectors;
+
+use RuntimeException;
+
+class MediaConnectorException extends RuntimeException
+{
+    public function __construct(
+        string $safeCode,
+        public readonly bool $needsReconnect = false,
+        public readonly ?int $retryAfterSeconds = null,
+    ) {
+        parent::__construct($safeCode);
+    }
+
+    public static function unauthorized(): self
+    {
+        return new self('connector_unauthorized', true);
+    }
+
+    public static function rateLimited(?int $retryAfterSeconds = null): self
+    {
+        return new self(
+            'connector_rate_limited',
+            false,
+            $retryAfterSeconds,
+        );
+    }
+
+    public static function requestFailed(): self
+    {
+        return new self('connector_request_failed');
+    }
+
+    public static function downloadFailed(): self
+    {
+        return new self('connector_download_failed');
+    }
+
+    public static function fileTooLarge(): self
+    {
+        return new self('connector_file_too_large');
+    }
+
+    public static function stagingFailed(): self
+    {
+        return new self('connector_staging_failed');
+    }
+}
