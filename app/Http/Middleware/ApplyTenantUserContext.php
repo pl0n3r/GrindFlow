f2c99751ac9f8fd1d580b2c13441d91473c9301c
Pipe Grind <drpipe1098@gmail.com>
2026-09-17 21:18:52 -0500

    refactor: move tenancy foundation to MariaDB

---
+++ app/Http/Middleware/ApplyTenantUserContext.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use App\Support\Tenancy\TenantContext;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApplyTenantUserContext
+{
+    public function __construct(private readonly TenantContext $tenantContext) {}
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        if (! ($user instanceof User)) {
+            return $next($request);
+        }
+
+        return $this->tenantContext->runAsUser(
+            $user,
+            fn (): Response => $next($request),
+        );
+    }
+}
