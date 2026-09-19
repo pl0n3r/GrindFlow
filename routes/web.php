0d05084a1934e12aeffcec8bafb869b285da738b
Pipe Grind <drpipe1098@gmail.com>
2026-09-18 19:58:35 -0500

    feat: route organization scheduler

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Connections\DropboxConnectionController;
 use App\Http\Controllers\Connections\GoogleDriveConnectionController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Scheduling\SchedulerController;
 use App\Http\Controllers\Vault\DirectUploadController;
 use App\Http\Controllers\Vault\VaultController;
 use Illuminate\Support\Facades\Route;
@@ -30,6 +31,10 @@ Route::middleware(['auth', 'tenant.user'])->group(function (): void {
                 ->name('organizations.vault.index');
             Route::post('/vault', [VaultController::class, 'store'])
                 ->name('organizations.vault.store');
+            Route::get('/scheduler', [SchedulerController::class, 'index'])
+                ->name('organizations.scheduler.index');
+            Route::post('/scheduler', [SchedulerController::class, 'store'])
+                ->name('organizations.scheduler.store');
             Route::post('/vault/direct-upload', [DirectUploadController::class, 'create'])
                 ->middleware('throttle:30,1')
                 ->name('organizations.vault.direct.create');
