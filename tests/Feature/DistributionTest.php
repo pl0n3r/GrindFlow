deae4f7c935d869ea6ce1af8f21278f529f5eac1
Pipe Grind <drpipe1098@gmail.com>
2026-09-19 01:07:02 -0500

    test(debug): reveal manual Distribution retry 404 origin

--- tests/Feature/DistributionTest.php
+++ tests/Feature/DistributionTest.php
@@ -106,6 +106,7 @@ class DistributionTest extends TestCase
             'deliveryId' => $delivery->getKey(),
         ]);
 
+        $this->withoutExceptionHandling();
         $this->actingAs($user)->post($route)->assertRedirect();
 
         $fresh = $this->delivery($delivery, $user, $organization);
