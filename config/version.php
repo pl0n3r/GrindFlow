bffce208389cc2eeb36ef7bb940e93f7047a2275
pl0n3r <64439547+pl0n3r@users.noreply.github.com>
2026-09-20 10:39:56 -0500

    feat(s1): integra admin privado y versión v0.1.34

--- config/version.php
+++ config/version.php
@@ -3,6 +3,6 @@
 // Deliberate human-readable GrindFlow product release.
 // This is NOT proof of the Git commit deployed on Hostinger.
 return [
-    'number' => '0.1.33',
+    'number' => '0.1.34',
     'released_at' => '2026-09-20',
 ];
