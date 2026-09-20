c17d3a41c49691232e98dc1f27c9d300f8a19444
pl0n3r <64439547+pl0n3r@users.noreply.github.com>
2026-09-20 16:05:35 -0500

    docs(release): filtro Vault por nombre y v0.1.47

--- config/version.php
+++ config/version.php
@@ -3,6 +3,6 @@
 // Deliberate human-readable GrindFlow product release.
 // This is NOT proof of the Git commit deployed on Hostinger.
 return [
-    'number' => '0.1.46',
+    'number' => '0.1.47',
     'released_at' => '2026-09-20',
 ];
