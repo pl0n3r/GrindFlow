757b96d8e8a495d872d5b6caaeb782fd66783a99
pl0n3r <64439547+pl0n3r@users.noreply.github.com>
2026-09-20 11:12:04 -0500

    test(mariadb): aísla DDL de fixtures de migración con claves foráneas

--- tests/Feature/SchedulingTest.php
+++ tests/Feature/SchedulingTest.php
@@ -616,8 +616,10 @@ class SchedulingTest extends TestCase
         $this->membership($user, $organization, UserRole::Editor);
         $this->membership($modelUser, $organization, UserRole::Model);
 
-        Schema::dropIfExists('scheduled_publications');
-        Schema::dropIfExists('publishing_destinations');
+        Schema::withoutForeignKeyConstraints(function (): void {
+            Schema::dropIfExists('scheduled_publications');
+            Schema::dropIfExists('publishing_destinations');
+        });
 
         $migrationPath = database_path(
             'migrations/2026_09_18_200000_create_scheduling_tables.php',
