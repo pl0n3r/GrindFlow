6663e65d9889dfc7269a468698d61e047282987c
pl0n3r <64439547+pl0n3r@users.noreply.github.com>
2026-09-23 02:44:22 -0500

    chore(release): preparar 0.1.108

--- config/version.php
+++ config/version.php
@@ -3,6 +3,6 @@
 // Deliberate human-readable GrindFlow product release.
 // This is NOT proof of the Git commit deployed on Hostinger.
 return [
-    'number' => '0.1.107',
+    'number' => '0.1.108',
     'released_at' => '2026-09-23',
 ];
